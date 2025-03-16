4d32a19f119ee7831ffca2f1bd23647b680beaa7
Avira George <avirageorge@Aviras-MacBook-Pro.local>
2025-03-16 13:28:26 +0000

    Made slight modification to gelocation result

--- backend/controllers/geoLocationController.js
+++ backend/controllers/geoLocationController.js
@@ -5,15 +5,19 @@ const {apiCall} = require("../utils/apiHelper");
 const getGeoLocation = async (req, res) => {
     try {
         const {query} = req.query;
-
-        if(!query) {
+        let finalResult = [];
+        if(!query) {    
             return responseGenerator(res, 400, false, "Invalid input", null);
         }
 
         //Generate url to make API call
-        let url = `${process.env.GEOLOCATION_API_BASE_URL}v1/search?name=${query}&count=10&language=en&format=json`;
+        let url = `${process.env.GEOLOCATION_API_BASE_URL}v1/search?name=${query}&count=1&language=en&format=json`;
         let searchResult = await apiCall(url, "GET", null, null);
-        return responseGenerator(res, 200, true, "Success", searchResult);
+        
+        if(searchResult && searchResult.results) {
+            finalResult = searchResult.results;
+        }
+        return responseGenerator(res, 200, true, "Success", finalResult);
 
     } catch(error) {
         return responseGenerator(res, 500, false, "Failed to fetch locations", null);
